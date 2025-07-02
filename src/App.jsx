88313e96147aa7bfe2f14cb832280c0597026c50
u7757 <ujefmokashi2005@gmail.com>
2025-07-02 15:35:54 +0530

    Fixed chatbot import path for Linux

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import AboutPage from "./pages/AboutPage";
 import CoursesPage from "./pages/CoursesPage";
 import AdmissionsPage from "./pages/AdmissionsPage";
 import NotFoundPage from "./pages/NotFoundPage";
-import ChatbotComponent from "./components/chatbot/chatbot";
+import ChatbotComponent from "./components/chatbot/chatbot.jsx";
 import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
 import { useState } from "react";git
 
