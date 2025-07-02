9642991c2f24ab04abd6c8db9fcc640ceabd2ecf
u7757 <ujefmokashi2005@gmail.com>
2025-07-02 11:40:31 +0530

    Updated entire project with latest changes

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+// src/App.jsx
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Header from "./components/Headers/Header";
+import Footer from "./components/Footer/Footer";
+import ContactPage from "./pages/ContactPage";
+import "./styles/pages.css";
+import AboutPage from "./pages/AboutPage";
+import CoursesPage from "./pages/CoursesPage";
+import AdmissionsPage from "./pages/AdmissionsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import ChatbotComponent from "./components/chatbot/chatbot";
+import DeveloperInfoPopup from "./components/DeveloperInfo/DeveloperInfoPopup";
+import { useState } from "react";
+
+function App() {
+  const [showPopup, setShowPopup] = useState(true);
+  const handleClosePopup = () => {
+    setShowPopup(false);
+  };
+  return (
+    <>
+    <div>
+        {/* Your main application content */}
+        <DeveloperInfoPopup
+          show={showPopup}
+          onClose={handleClosePopup}
+          studentName="Ujef Mokashi"
+          studentPhotoUrl="/images/saniya.jpg" // Path to their photo
+          uniqueMessage="Learned so much during this OJT! This app showcases my independent coding and deployment skills"
+        />
+      </div> 
+    <Router>
+      <div className="main-layout">
+        <Header />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/courses" element={<CoursesPage />} />
+            <Route path="/admissions" element={<AdmissionsPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <ChatbotComponent />
+        </div>
+        <Footer />
+      </div>
+    </Router>
+    </>
+  );
+}
+export default App;
+
+
